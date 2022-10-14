4d8adbb20f81e86d11d64276d5b7a3060719d59c
Karnchai-Van <Sakkarnjana@outlook.com>
2022-10-14 14:24:38 +0700

    Add Page and resource in server root

---
+++ PixelCost.Service.Identity/Models/Upload/WalletDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace PixelCost.Service.Model.DTOModel
+{
+    public class WalletDTO
+    {
+        public string UserID { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string JobTitle { get; set; }
+        public decimal? Balance { get; set; }
+        public decimal? TotalExpense { get; set; }
+        public int TotalNumberExpense { get; set; }
+        public decimal? TotalRevenue { get; set; }
+        public int TotalNumberRevenue { get; set; }
+        public DateTime? DateCreate { get; set; }
+    }
+}
---
+++ PixelCost.Service.Identity/Pages/Account/EmailConfirmation/InvalidEmail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PixelCost.Service.Identity.Pages.Account.EmailConfirmation
+{
+    public class InvalidEmailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
