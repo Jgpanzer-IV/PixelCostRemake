d8584f7cd2c33de88b99c1394f910348684516eb
Karnchai-Van <Sakkarnjana@outlook.com>
2022-10-15 20:51:37 +0700

    Add services abstract and implementation

--- PixelCost.Service.WalletAPI/Program.cs
+++ PixelCost.Service.WalletAPI/Program.cs
@@ -1,26 +1,35 @@
+using AutoMapper;
 using PixelCost.Service.WalletAPI.Database;
+using PixelCost.Service.WalletAPI.Model.DTOs;
+using PixelCost.Service.WalletAPI.Model.Entities;
+using PixelCost.Service.WalletAPI.Services.Implementations;
+using PixelCost.Service.WalletAPI.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 
-
-builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration.GetConnectionString("SqlServer"));
-
-
-
-
-
-
-
-
-
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration.GetConnectionString("SqlServer"));
+
+builder.Services.AddScoped<IWalletRepository, WalletRepository>();
+
+IMapper MapperConfiguration = new MapperConfiguration(express =>
+{
+    express.CreateMap<Wallet, WalletDTO>();
+    express.CreateMap<PaymentMethod, PaymentMethodDTO>();
+
+}).CreateMapper();
+builder.Services.AddSingleton(MapperConfiguration); 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
 
 var app = builder.Build();
 
---
+++ PixelCost.Service.WalletAPI/Services/Interface/IPaymentMethodRepository.cs
@@ -0,0 +1,13 @@
+﻿
+using PixelCost.Service.WalletAPI.Model.DTOs;
+
+namespace PixelCost.Service.WalletAPI.Services.Interfaces
+{
+    public interface IPaymentMethodRepository
+    {
+        Task<List<PaymentMethodDTO>?> RetrieveByWalletIdAsync(string walletId);
+        Task<PaymentMethodDTO?> CreateAsync(PaymentMethodDTO paymentMethodDTO);
+        Task<PaymentMethodDTO?> UpdateAsync(PaymentMethodDTO paymentMethodDTO);
+        Task<bool> DeleteAsync(long Id);
+    }
+}
