8722fb06b793e7682bfa598ac0be2749182b68dc
Karnchai-Van <Sakkarnjana@outlook.com>
2022-10-16 03:48:24 +0700

    Add Services and Implementations

--- PixelCost.Service.PaymentAPI/Program.cs
+++ PixelCost.Service.PaymentAPI/Program.cs
@@ -1,5 +1,10 @@
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using PixelCost.Service.PaymentAPI.Database;
+using PixelCost.Service.PaymentAPI.Models.DTOs;
+using PixelCost.Service.PaymentAPI.Models.Entities;
+using PixelCost.Service.PaymentAPI.Services.Implementations;
+using PixelCost.Service.PaymentAPI.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,12 +20,17 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 
 
+builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
 
 
-
-
-
-
+IMapper MapperService = new MapperConfiguration(config => {
+    config.CreateMap<Expense, ExpenseDTO>().ReverseMap();
+    config.CreateMap<PrimaryExpense, PrimaryExpenseDTO>().ReverseMap();
+    config.CreateMap<Revenue, RevenueDTO>().ReverseMap();
+    config.CreateMap<PaymentMethod, PaymentMethodDTO>().ReverseMap();
+}).CreateMapper();
+builder.Services.AddSingleton(MapperService);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 
 var app = builder.Build();
