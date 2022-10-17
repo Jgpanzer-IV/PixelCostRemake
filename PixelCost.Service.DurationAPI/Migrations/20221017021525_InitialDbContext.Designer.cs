1ddfb04829db2d1a90e39006d4de37e2e3a8ca27
Karnchai-Van <Sakkarnjana@outlook.com>
2022-10-17 10:41:22 +0700

    Add Controller with configuring accept type for xml and json

--- PixelCost.Service.DurationAPI/Migrations/20221017021525_InitialDbContext.Designer.cs
+++ PixelCost.Service.DurationAPI/Migrations/20221017021525_InitialDbContext.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using PixelCost.DurationAPI.Database;
+using PixelCost.Service.DurationAPI.Database;
 
 #nullable disable
 
--- PixelCost.Service.DurationAPI/Program.cs
+++ PixelCost.Service.DurationAPI/Program.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using PixelCost.Service.DurationAPI.Database;
@@ -11,7 +12,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddXmlDataContractSerializerFormatters()
+    .AddXmlSerializerFormatters();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(configAction => {
