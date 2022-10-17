629d2962057b0032773e51dbd052e7b1a72a2acd
Karnchai-Van <Sakkarnjana@outlook.com>
2022-10-17 10:21:53 +0700

    Add services with registering and Swagger documentation

--- PixelCost.Service.DurationAPI/Program.cs
+++ PixelCost.Service.DurationAPI/Program.cs
@@ -1,5 +1,11 @@
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
-using PixelCost.DurationAPI.Database;
+using Microsoft.OpenApi.Models;
+using PixelCost.Service.DurationAPI.Database;
+using PixelCost.Service.DurationAPI.Models.DTOs;
+using PixelCost.Service.DurationAPI.Models.Entities;
+using PixelCost.Service.DurationAPI.Services.Implementations;
+using PixelCost.Service.DurationAPI.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,15 +14,42 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(configAction => {
+    configAction.SwaggerDoc("v1", new OpenApiInfo {
+        Version = "v1",
+        Title = "Duration API",
+        Description = "This api responsible to serve the 'Duration' entity and its releated infomation that using orther entity.",
+        Contact = new OpenApiContact
+        {
+            Name = "Karnchai Sakkarnjana",
+            Email = "Sakkarnjana@outlook.com",
+            Url = new Uri(builder.Configuration["AdminContect"])
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Pixel License",
+            Url = new Uri(builder.Configuration["ClientUrl:PixelLicense"].ToString())
+        },
+        TermsOfService = new Uri(builder.Configuration["ClientUrl:TermOfService"].ToString())
+    });
+});
 
 
 builder.Services.AddDbContext<ApplicationDbContext>(config => config.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 
+builder.Services.AddScoped<IDurationRepository, DurationRepository>();
 
+IMapper MapperService = new MapperConfiguration(config => {
 
+    config.CreateMap<Duration, DurationDTO>().ReverseMap();
+    config.CreateMap<SubDuration, SubDurationDTO>().ReverseMap();
+    config.CreateMap<Category, CategoryDTO>().ReverseMap();
+    config.CreateMap<PrimaryExpense, PrimaryExpenseDTO>().ReverseMap();
+    config.CreateMap<Revenue, RevenueDTO>().ReverseMap();
 
-
+}).CreateMapper();
+builder.Services.AddSingleton(MapperService);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 
 var app = builder.Build();
