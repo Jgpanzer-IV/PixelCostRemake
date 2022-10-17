df75e8ea2cdafbfcbce2a28d6d6443feff98d708
Karnchai-Van <Sakkarnjana@outlook.com>
2022-10-17 09:48:14 +0700

    Add DbContext and Configuration connection string

---
+++ PixelCost.Service.DurationAPI/Database/ApplicationDbContext.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PixelCost.Service.DurationAPI.Models.Entities;
+
+namespace PixelCost.Service.DurationAPI.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {}
+
+        public DbSet<Duration>? Durations { get; set; }
+        public DbSet<SubDuration>? SubDurations { get; set; }
+        public DbSet<PrimaryExpense>? PrimaryExpenses { get; set; }
+        public DbSet<Revenue>? Revenues { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+            modelBuilder.Entity<Duration>(buildAction =>
+            {
+                buildAction.ToTable("Durations");
+                buildAction.HasKey(e => e.Id);
+                buildAction.Property(e => e.UserId).IsRequired();
+                buildAction.Property(e => e.Name).IsRequired().HasMaxLength(32);
+                buildAction.Property(e => e.StartingDate).IsRequired();
+                buildAction.Property(e => e.EndingDate).IsRequired();
+                buildAction.Property(e => e.TotalDays);
+                buildAction.Property(e => e.RemainingDays);
+                buildAction.Property(e => e.Progress);
+                buildAction.Property(e => e.InitialCost).IsRequired().HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.TotalCost).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.SumSubDurationCost).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.SumCategoryCost).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.UsableMoney).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.Revenue).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.RevenueCount);
+                buildAction.Property(e => e.Expense).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.ExpenseCount);
+                buildAction.Property(e => e.Balance).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.SumSubDurationBalance).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.SumCategoryBalance).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.IsActive).HasDefaultValue(value: true);
+
+                buildAction.HasMany(e => e.SubDurations).WithOne(e => e.Duration).HasForeignKey(e => e.DurationId).IsRequired();
+                buildAction.HasMany(e => e.Revenues).WithOne(e => e.Duration).HasForeignKey(e => e.DurationId).IsRequired();
+                buildAction.HasMany(e => e.PrimaryExpenses).WithOne(e => e.Duration).HasForeignKey(e => e.DurationId).IsRequired();
+                buildAction.HasMany(e => e.Categories).WithOne(e => e.Duration).HasForeignKey(e => e.DurationId).IsRequired();
+            });
+
+            modelBuilder.Entity<SubDuration>(buildAction => {
+                buildAction.ToTable("SubDurations");
+                buildAction.HasKey(e => e.Id);
+
+                buildAction.Property(e => e.Name).IsRequired().HasMaxLength(32);
+                buildAction.Property(e => e.StartingDate).IsRequired();
+                buildAction.Property(e => e.EndingDate).IsRequired();
+                buildAction.Property(e => e.TotalDays);
+                buildAction.Property(e => e.RemainingDays);
+                buildAction.Property(e => e.Progress);
+                buildAction.Property(e => e.InitialCost).HasColumnType("money").HasConversion<double>().IsRequired();
+                buildAction.Property(e => e.TotalCost).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.SumCategoryCost).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.UsableMoney).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.Revenue).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.RevenueCount);
+                buildAction.Property(e => e.Expense).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.ExpenseCount);
+                buildAction.Property(e => e.Balance).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.SumCategoryBalance).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.IsAchived).HasDefaultValue(true);
+
+                buildAction.HasOne(e => e.Duration).WithMany(e => e.SubDurations).HasForeignKey(e => e.DurationId).IsRequired();
+            });
+
+            modelBuilder.Entity<Category>(buildAction =>
+            {
+                buildAction.ToTable("Categories");
+                buildAction.HasKey(e => e.Id);
+
+                buildAction.Property(e => e.Name).IsRequired();
+                buildAction.Property(e => e.Cost).HasColumnType("money").HasConversion<double>().IsRequired();
+                buildAction.Property(e => e.Expense).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.ExpenseCount);
+                buildAction.Property(e => e.Balance).HasColumnType("money").HasConversion<double>();
+                buildAction.Property(e => e.IsAchived).HasDefaultValue(true);
+
+                buildAction.HasOne(e=>e.Duration).WithMany(e=>e.Categories).HasForeignKey(e=>e.DurationId).IsRequired();
+            });
+            
+            modelBuilder.Entity<PrimaryExpense>(buildAction => {
+                buildAction.ToTable("PrimaryExpenses");
+                buildAction.HasKey(e => e.Id);
+
+                buildAction.Property(e => e.OrderingName).IsRequired();
+                buildAction.Property(e => e.OrderingDate).IsRequired();
+                buildAction.Property(e => e.OrderingPrice).HasColumnType("money").HasConversion<double>().IsRequired();
+
+                buildAction.HasOne(e=>e.Duration).WithMany(e=>e.PrimaryExpenses).HasForeignKey(e => e.DurationId).IsRequired();
+            });
+
+            modelBuilder.Entity<Revenue>(buildAction => {
+                buildAction.ToTable("Revenues");
+                buildAction.HasKey(e => e.Id);
+
+                buildAction.Property(e => e.Task).IsRequired();
+                buildAction.Property(e => e.EarningAmount).HasColumnType("money").HasConversion<double>().IsRequired();
+                buildAction.Property(e => e.EarningDate).IsRequired();
+
+                buildAction.HasOne(e=>e.Duration).WithMany(e=>e.Revenues).HasForeignKey(e => e.DurationId).IsRequired();
+                
+            });
+        
+        }
+
+    }
+}
---
+++ PixelCost.Service.DurationAPI/Migrations/20221017021525_InitialDbContext.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PixelCost.Service.DurationAPI.Migrations
+{
+    public partial class InitialDbContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Durations",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    StartingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TotalDays = table.Column<int>(type: "int", nullable: true),
+                    RemainingDays = table.Column<int>(type: "int", nullable: true),
+                    Progress = table.Column<float>(type: "real", nullable: true),
+                    InitialCost = table.Column<decimal>(type: "money", nullable: false),
+                    TotalCost = table.Column<decimal>(type: "money", nullable: true),
+                    SumCategoryCost = table.Column<decimal>(type: "money", nullable: true),
+                    SumSubDurationCost = table.Column<decimal>(type: "money", nullable: true),
+                    UsableMoney = table.Column<decimal>(type: "money", nullable: true),
+                    Revenue = table.Column<decimal>(type: "money", nullable: true),
+                    RevenueCount = table.Column<int>(type: "int", nullable: true),
+                    Expense = table.Column<decimal>(type: "money", nullable: true),
+                    ExpenseCount = table.Column<int>(type: "int", nullable: true),
+                    Balance = table.Column<decimal>(type: "money", nullable: true),
+                    SumCategoryBalance = table.Column<decimal>(type: "money", nullable: true),
+                    SumSubDurationBalance = table.Column<decimal>(type: "money", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: true, defaultValue: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Durations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cost = table.Column<decimal>(type: "money", nullable: false),
+                    Expense = table.Column<decimal>(type: "money", nullable: true),
+                    ExpenseCount = table.Column<int>(type: "int", nullable: true),
+                    Balance = table.Column<decimal>(type: "money", nullable: true),
+                    IsAchived = table.Column<bool>(type: "bit", nullable: true, defaultValue: true),
+                    DurationId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Categories_Durations_DurationId",
+                        column: x => x.DurationId,
+                        principalTable: "Durations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PrimaryExpenses",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderingName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrderingPrice = table.Column<decimal>(type: "money", nullable: false),
+                    OrderingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DurationId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PrimaryExpenses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PrimaryExpenses_Durations_DurationId",
+                        column: x => x.DurationId,
+                        principalTable: "Durations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Revenues",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Task = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EarningAmount = table.Column<decimal>(type: "money", nullable: false),
+                    EarningDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DurationId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Revenues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Revenues_Durations_DurationId",
+                        column: x => x.DurationId,
+                        principalTable: "Durations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubDurations",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    StartingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TotalDays = table.Column<int>(type: "int", nullable: true),
+                    RemainingDays = table.Column<int>(type: "int", nullable: true),
+                    Progress = table.Column<float>(type: "real", nullable: true),
+                    InitialCost = table.Column<decimal>(type: "money", nullable: false),
+                    TotalCost = table.Column<decimal>(type: "money", nullable: true),
+                    SumCategoryCost = table.Column<decimal>(type: "money", nullable: true),
+                    UsableMoney = table.Column<decimal>(type: "money", nullable: true),
+                    Revenue = table.Column<decimal>(type: "money", nullable: true),
+                    RevenueCount = table.Column<int>(type: "int", nullable: true),
+                    Expense = table.Column<decimal>(type: "money", nullable: true),
+                    ExpenseCount = table.Column<int>(type: "int", nullable: true),
+                    Balance = table.Column<decimal>(type: "money", nullable: true),
+                    SumCategoryBalance = table.Column<decimal>(type: "money", nullable: true),
+                    IsAchived = table.Column<bool>(type: "bit", nullable: true, defaultValue: true),
+                    DurationId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubDurations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubDurations_Durations_DurationId",
+                        column: x => x.DurationId,
+                        principalTable: "Durations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_DurationId",
+                table: "Categories",
+                column: "DurationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PrimaryExpenses_DurationId",
+                table: "PrimaryExpenses",
+                column: "DurationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Revenues_DurationId",
+                table: "Revenues",
+                column: "DurationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubDurations_DurationId",
+                table: "SubDurations",
+                column: "DurationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "PrimaryExpenses");
+
+            migrationBuilder.DropTable(
+                name: "Revenues");
+
+            migrationBuilder.DropTable(
+                name: "SubDurations");
+
+            migrationBuilder.DropTable(
+                name: "Durations");
+        }
+    }
+}
