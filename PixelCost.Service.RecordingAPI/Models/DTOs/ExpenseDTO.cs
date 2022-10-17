67d28d1ca172f74d335dc413658708fb2b024747
Karnchai-Van <Sakkarnjana@outlook.com>
2022-10-17 22:24:59 +0700

    Add DTO models

---
+++ PixelCost.Service.RecordingAPI/Models/DTOs/ExpenseDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace PixelCost.Service.RecordingAPI.Models.DTOs
+{
+    public class ExpenseDTO
+    {
+        public long Id { get; set; }
+        public string OrderingName { get; set; } = string.Empty;
+        public decimal OrderingPrice { get; set; }
+        public DateTime OrderingDate { get; set; }
+
+        public string UserId { get; set; } = string.Empty;
+        public long DurationId { get; set; }
+        public long SubDurationId { get; set; }
+        public long CategoryId { get; set; }
+    }
+}
---
+++ PixelCost.Service.RecordingAPI/Models/DTOs/RevenueDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace PixelCost.Service.RecordingAPI.Models.DTOs
+{
+    public class RevenueDTO
+    {
+        public long Id { get; set; }
+        public string Task { get; set; } = string.Empty;
+        public decimal EarningAmount { get; set; }
+        public DateTime EarningDate { get; set; }
+
+        public string UserId { get; set; } = string.Empty;
+        public long DurationId { get; set; }
+        public long SubDurationId { get; set; }
+        public long CategoryId { get; set; }
+
+    }
+}
